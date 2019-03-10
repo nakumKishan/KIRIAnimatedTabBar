50377d88705a7e74cb7bd74e7bda53d0b60dbfd9
Kishan Nakum <kishannakum@Kishans-MacBook-Pro.local>
2019-03-10 14:31:57 +0530

    Added Xcode Project to Repo

---
+++ KIRIAnimatedTabBar/KIRIAnimatedTabBar/KIRIAnimatedTabBar.h
@@ -0,0 +1,19 @@
+//
+//  KIRIAnimatedTabBar.h
+//  KIRIAnimatedTabBar
+//
+//  Created by Kishan Nakum on 10/03/19.
+//  Copyright © 2019 Kishan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KIRIAnimatedTabBar.
+FOUNDATION_EXPORT double KIRIAnimatedTabBarVersionNumber;
+
+//! Project version string for KIRIAnimatedTabBar.
+FOUNDATION_EXPORT const unsigned char KIRIAnimatedTabBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KIRIAnimatedTabBar/PublicHeader.h>
+
+
